c0448f82ce739e6032ccc5e75a23c41eea74b9e0
Farmstead001 <131138226+Farmstead001@users.noreply.github.com>
2024-02-22 17:53:52 +0100

    first draft of event list

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+const events = [
+    {Name: 'Maks-klubben', Location: "Stjørdal Bedehus", Date: '21/02/2024', Time: '17:00-19:30', AgeGroup: '2-7. Trinn', Description: 'En plass der man kan være seg selv, og lære om jesus og gud.'},
+    {Name: 'SHARE', Location: "Stjørdal Misjonskirke", Date: '23/02/2024', Time: '17:00-19:00', AgeGroup: '5-7. Trinn', Description: 'En plass der man kan være seg selv, og lære om jesus og gud.'},
+    {Name: 'FUEL', Location: "Stjørdal Misjonskirke", Date: '23/02/2024', Time: '20:00-23:00', AgeGroup: '8-10. Trinn', Description: 'en deskripsjon'}
+]
+
+</script>
+
+<template>
+    <div class="event-list">
+        <li v-for="event in events" :key="event.id" style="list-style-type: none;" class="event-item">
+            <div class="event-header">
+                <h1>{{ event.Name }}</h1>
+                <h4>{{ event.Location }}</h4>
+            </div>
+            <div class="event-info">
+                <h4>{{ event.Time }}</h4>
+                <h4>{{ event.AgeGroup }}</h4>
+            </div>
+            <p>{{ event.Description }}</p>
+        </li>
+    </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+import 'https://requirejs.org/docs/release/2.3.5/minified/require.js'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
