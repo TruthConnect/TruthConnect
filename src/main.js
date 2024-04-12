334a19e6f15edd7b76c2e8152b142b5cd7fef6c5
Farmstead001 <131138226+Farmstead001@users.noreply.github.com>
2024-04-12 07:09:21 +0000

    cleared the project

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import './assets/main.css'
-import 'https://requirejs.org/docs/release/2.3.5/minified/require.js'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
